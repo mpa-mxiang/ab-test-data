5071f064c4ffd47997d23454aec16b1ee02f0a4a
anaymous <max997123@gmail.com>
2023-12-03 13:14:03 -0500

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,15 @@
+/* What are the start and end dates of the experiment? */
+/* Start date: */
+SELECT * FROM activity ORDER BY dt LIMIT 1;
+
+
+
+
+
+
+
+
+
+/* End date: */
+SELECT * FROM activity ORDER BY dt DESC LIMIT 1;
+
