cd24ecf39cb8c578f08e3fba4f11a673497fbac6
anaymous <max997123@gmail.com>
2023-12-09 22:34:41 -0500

    add one query to download as csv for later touch

--- queries.sql
+++ queries.sql
@@ -57,3 +57,32 @@ LEFT JOIN
     activity a ON g.uid = a.uid
 GROUP BY
     g.group;
+
+
+
+
+
+/*
+Write a SQL query that returns: the user ID,
+the user’s country, the user’s gender, the user’s
+device type, the user’s test group, whether or not
+they converted (spent > $0), and how much they spent
+in total ($0+). 
+*/
+
+SELECT
+    u.id AS user_id,
+    u.country,
+    u.gender,
+    COALESCE(a.device, 'Unknown') AS device,
+    g.group AS test_group,
+    CASE WHEN a.spent > 0 THEN 'Yes' ELSE 'No' END AS converted,
+    COALESCE(SUM(a.spent), 0) AS total_spent
+FROM
+    users u
+LEFT JOIN
+    groups g ON u.id = g.uid
+LEFT JOIN
+    activity a ON u.id = a.uid
+GROUP BY
+    u.id, u.country, u.gender, a.device, g.group, a.spent;
